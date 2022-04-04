1591b342bbe365035583f3ec9febe1c207354029
KanoCode <kanokanombola@gmail.com>
2022-04-04 22:07:52 +0200

    Update styles.css, index.html

--- index.js
+++ index.js
@@ -4,20 +4,26 @@
 
 const navBtn = document.querySelector(".nav-mobile > .btn");
 const activeHeader = document.querySelector("header");
+const navUl = document.querySelectorAll(".nav-desktop li");
+
 
 const barsBtn = document.querySelector(".btn > .fa-solid");
 
 //event handler
 function showHiddenMenu() {
   if (activeHeader.classList.contains("active-header")) {
-    activeHeader.classList.toggle("active-header");
+    activeHeader.classList.remove("active-header");
     barsBtn.classList.replace("fa-times","fa-bars")
   } else {
-    activeHeader.classList.toggle("active-header");
+    activeHeader.classList.add("active-header");
     barsBtn.classList.replace("fa-bars","fa-times");
   }
 }
 
 //register event handler
+navUl.forEach(a => { a.addEventListener('click',function(){
+    activeHeader.classList.remove('active-header');
+    barsBtn.classList.replace("fa-times","fa-bars");
+    })})
 
 navBtn.addEventListener("click", showHiddenMenu);
