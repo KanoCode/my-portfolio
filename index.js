5950281aac49e6603ad7b221f0d61e78f4941b37
KanoCode <kanokanombola@gmail.com>
2022-04-04 22:22:38 +0200

    Fix linters

--- index.js
+++ index.js
@@ -2,28 +2,29 @@
 
 // hamburger menu
 
-const navBtn = document.querySelector(".nav-mobile > .btn");
-const activeHeader = document.querySelector("header");
-const navUl = document.querySelectorAll(".nav-desktop li");
+const navBtn = document.querySelector('.nav-mobile > .btn');
+const activeHeader = document.querySelector('header');
+const navUl = document.querySelectorAll('.nav-desktop li');
 
+const barsBtn = document.querySelector('.btn > .fa-solid');
 
-const barsBtn = document.querySelector(".btn > .fa-solid");
-
-//event handler
+// event handler
 function showHiddenMenu() {
-  if (activeHeader.classList.contains("active-header")) {
-    activeHeader.classList.remove("active-header");
-    barsBtn.classList.replace("fa-times","fa-bars")
+  if (activeHeader.classList.contains('active-header')) {
+    activeHeader.classList.remove('active-header');
+    barsBtn.classList.replace('fa-times', 'fa-bars');
   } else {
-    activeHeader.classList.add("active-header");
-    barsBtn.classList.replace("fa-bars","fa-times");
+    activeHeader.classList.add('active-header');
+    barsBtn.classList.replace('fa-bars', 'fa-times');
   }
 }
 
-//register event handler
-navUl.forEach(a => { a.addEventListener('click',function(){
+// register event handler
+navUl.forEach((a) => {
+  a.addEventListener('click', () => {
     activeHeader.classList.remove('active-header');
-    barsBtn.classList.replace("fa-times","fa-bars");
-    })})
+    barsBtn.classList.replace('fa-times', 'fa-bars');
+  });
+});
 
-navBtn.addEventListener("click", showHiddenMenu);
+navBtn.addEventListener('click', showHiddenMenu);
