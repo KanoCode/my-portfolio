683bb41c71c37fa61de5cc1961ef08217bd48121
KanoCode <kanokanombola@gmail.com>
2022-04-05 15:30:47 +0200

    Create dynamic cards

--- index.js
+++ index.js
@@ -28,3 +28,73 @@ navUl.forEach((a) => {
 });
 
 navBtn.addEventListener('click', showHiddenMenu);
+
+
+// Generate card content dynamically
+
+
+const cardsObj = [
+
+{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+}
+,{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+},{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+
+},{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+},{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+},{
+  title: "Multi-Post Stories Gain+Glory",
+  technologies: ["Ruby on rails", "CSS", "JavaSc0ript", "HTML"]
+  ,projectBtn : "See Project",
+},
+]
+
+//get card container
+
+const cardContainer = document.querySelector(".card-container");
+
+
+//create card
+
+const cardTemplate = cardsObj.forEach((obj)=>{
+  let template = document.createElement('ul');
+template.innerHTML = `<li class="card-work">
+<div class="img-placeholder"></div>
+<div class="card-details">
+  <div class="card-description">
+    <h2>${obj.title}</h2>
+  </div>
+  <ul class="languages">
+    <li><h3>${obj.technologies[0]}</h3></li>
+    <li><h3>${obj.technologies[1]}</h3></li>
+    <li><h3>${obj.technologies[2]}</h3></li>
+    <li><h3>${obj.technologies[3]}</h3></li>
+  </ul>
+  <button type="button" class="see-project">${obj.projectBtn}</button>
+</div>
+</li>`
+cardContainer.appendChild(template);
+
+
+}) 
+
+//create popup 
+
+
+
+
